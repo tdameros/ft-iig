3d3847e84c77876e72370ababbca432e25a4a475
BenoitRoux0 <102321220+BenoitRoux0@users.noreply.github.com>
2022-10-12 20:29:35 +0200

    Delete launcher.py

--- launcher.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-if __name__ == "__main__":
-    git_pull = subprocess.Popen(["git", "pull", "origin", "main"],
-                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    git_pull.wait()
-    exec(open("main.py").read())
